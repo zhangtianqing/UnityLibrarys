e8c33518a5a0ca175ef872d3d751de656e4d9bf1
zhangtianqing <a1092484359@yeah.net>
2022-05-31 11:23:45 +0800

    Update Script
    规范化命名
    添加竖向文字排版脚本

---
+++ Utils/Singleton/SingletonClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SingletonClass<T>   where T : SingletonClass<T>,new()
+{
+    private static T _instance;
+    private static object o = new object();
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (o)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new T();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Utils/Singleton/SingletonMono.cs
@@ -0,0 +1,28 @@
+﻿// CSharp单例
+using UnityEngine;
+public class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
+{
+    private static T _instance;
+    private static object o=new object();
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+
+                _instance = FindObjectOfType<T>();
+
+                lock (o)
+                {
+                    if (_instance == null)
+                    {
+                	        _instance = new GameObject(typeof(T).ToString()).AddComponent<T>();
+		            
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
