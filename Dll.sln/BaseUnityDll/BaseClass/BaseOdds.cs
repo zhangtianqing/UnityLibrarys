6ee5eaa9ed8e0bc5ee09f92d20d91f57ed8e34c2
zhangtianqing <a1092484359@yeah.net>
2024-07-01 11:10:38 +0800

    commit changed

---
+++ Dll.sln/BaseUnityDll/BaseClass/BaseOdds.cs
@@ -0,0 +1,45 @@
+﻿namespace BaseUnityDll.BaseClass
+{
+
+    public abstract class BaseOdds<T>
+    {
+
+        public float start;
+        public bool withStart = false;
+
+        public float end;
+        public bool withEnd = false;
+
+        public bool toEnd = false;
+        public T value;
+        public void TimeDecode(string input)
+        {
+            withStart = input.StartsWith("[");
+            withEnd = input.EndsWith("]");
+            string str = input.Substring(0, input.Length - 1);
+            str = str.Substring(1, str.Length - 1);
+            string[] vs1 = str.Split('-');
+            start = float.Parse(vs1[0]);
+            if (vs1[1] == "End")
+            {
+                end = float.MaxValue;
+            }
+            else
+            {
+                end = float.Parse(vs1[1]);
+            }
+        }
+        public bool CheckByTime(float time)
+        {
+            return start <= time && time < end;
+        }
+        protected abstract void ValueDecode(string[] input);
+        protected void DecodeData(string value)
+        {
+            string[] statePara = value.Split(',');
+            TimeDecode(statePara[0]);
+            ValueDecode(statePara);
+
+        }
+    }
+}
